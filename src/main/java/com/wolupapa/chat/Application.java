adf9a1573659a900a266510359f7ec0d62c6c03f
Ruiyang Wang <895437987@qq.com>
2023-11-10 09:21:50 +0800

    feat: 添加Springboot启动类&配置端口号为80

---
+++ src/main/java/com/wolupapa/chat/Application.java
@@ -0,0 +1,17 @@
+package com.wolupapa.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author wrystart
+ * @Version 1.0
+ * @Date 2023/11/09 20:53
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
